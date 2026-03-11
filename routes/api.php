4c4d81a43b1382a9967e26d3d5ef00de2e524664
AdairVazquez <adairvazquezcr@gmail.com>
2026-03-11 10:37:57 -0600

    importacion de la clase en las rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\BranchController;
+use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
